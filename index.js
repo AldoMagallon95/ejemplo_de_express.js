32962a5ca53c7b3a0357f0e17148443cd73d5bf6
Aldo <aldomaster95@gmail.com>
2021-10-10 17:22:48 -0500

    clase 7

---
+++ index.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const faker = require('faker');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Mi server en express');
+});
+
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Nueva ruta');
+});
+
+app.get('/home', (req, res) => {
+  res.send('home');
+});
+
+app.get('/products', (req, res) => {
+  const products = [];
+  for (let index = 0; index < 100; index++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.imageUrl(),
+    })
+  };
+  res.json(products);
+});
+
+app.get('/products/:id', (req, res) => {
+  const {id} = req.params;
+  res.json({
+    id,
+    name: 'Product 2',
+    price: 2000
+  })
+})
+
+app.get('/users', (req, res) => {
+  const {limit, offset} = req.query;
+  if(limit && offset) {
+    res.json({
+      limit,
+       offset
+      });
+  } else {
+    res.send('No hay parametros');
+  }
+
+})
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const {categoryId, productId } = req.params;
+  res.json({
+    categoryId,
+    productId
+  })
+})
+
+app.get('/user/:userId/', (req, res) => {
+  const {userId} = req.params;
+  res.json({
+    userId,
+    Country: 'Mexico',
+    Name: 'Aldo'
+  })
+})
+
+app.get('/productDescription/:descriptionId/', (req, res) => {
+  const {descriptionId } = req.params;
+  res.json({
+    descriptionId,
+    name: 'Product1',
+    price: '3000',
+    Arrival: '3 days'
+  })
+})
+
+
+app.listen(port, () => {
+  console.log("Mi port" + port )
+});
+
