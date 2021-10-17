58a9f85551dddd826938896bd89ea5a0a1255a70
Aldo <aldomaster95@gmail.com>
2021-10-17 17:07:18 -0500

    clase 22

--- schemas/product.schema.js
+++ schemas/product.schema.js
@@ -1,17 +1,20 @@
 const Joi = require('Joi');
 
 const id = Joi.string().uuid();
-const name = Joi.string().alphanum.min(3).max(15);
+const name = Joi.string().min(3).max(15);
 const price = Joi.number().integer().min(10)
+const image = Joi.string().uri();
 
 const createProductSchema = Joi.object({
   name: name.required(),
   price: price.required(),
+  image: image.required(),
 })
 
 const updateProductSchema = Joi.object({
-  name: name.required(),
-  price: price.required(),
+  name: name,
+  price: price,
+  image: image,
 })
 
 
@@ -19,4 +22,4 @@ const getProductSchema = Joi.object({
   id: id.required(),
 })
 
-module.exports = {createProductSchema,updateProductSchema, getProductSchema }
+module.exports = {createProductSchema,updateProductSchema, getProductSchema  }
