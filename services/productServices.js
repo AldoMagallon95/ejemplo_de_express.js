17694b5fae3922bbc4c42e05d9f9b4f8b34d04b4
Aldo <aldomaster95@gmail.com>
2021-10-17 15:53:32 -0500

    clase 18

--- services/productServices.js
+++ services/productServices.js
@@ -1,4 +1,5 @@
 const faker = require('faker');
+const boom = require('@hapi/boom');
 
 class ProductsService {
 
@@ -16,6 +17,7 @@ class ProductsService {
       name: faker.commerce.productName(),
       price: parseInt(faker.commerce.price(), 10),
       image: faker.image.imageUrl(),
+      isBlocked: faker.datatype.boolean(),
     })
   }
 }
@@ -31,7 +33,7 @@ class ProductsService {
 
 
   find(){
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
       setTimeout(() => {
         resolve(this.products);
       },5000)
@@ -39,14 +41,20 @@ class ProductsService {
   }
 
   async findOne(id) {
-    const name = this.getTotal();
-    return this.products.find(item => item.id === id);
+    const product = this.products.find(item => item.id === id);
+    if(!product){
+      throw boom.notFound('Product not found');
+    }
+    if(product.isBlocked){
+      throw boom.conflict('Product is blocked');
+    }
+    return product;
   }
 
   async update(id, changes) {
     const index = this.products.findIndex(item => item.id === id);
     if (index === -1) {
-      throw new Error('product not found');
+      throw boom.notFound('Product not found');
     }
     const product = this.products[index];
     this.products[index] = {
